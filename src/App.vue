44cd68356db69c276d3914b16d02d1fd2683553b
liuxiangdong <liuxiangdong@songguo7.com>
2021-05-11 20:52:52 +0800

    feat: 搭建页面

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <BorderRadius />
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>
+          <HelloWorld msg="Welcome to Your Vue.js App" />
+          <BorderRadius />
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import BorderRadius from './components/BorderRadius.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import BorderRadius from "./components/BorderRadius.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    BorderRadius
-  }
-}
+    BorderRadius,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +35,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
